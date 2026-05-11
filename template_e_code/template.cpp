ca14765868edafc58b0a17b78686195c68f3ccc8
Caio <caiodiasfleury@gmail.com>
2026-05-11 15:21:59 -0300

    Update template.cpp

---
+++ template_e_code/template.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+#define todo(x) x.begin(), x.end()
+#define rtodo(x) x.rbegin(), x.rend()
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define sz(x) (int)x.size()
+
+#define rep(i,a,b) for(int i = (a); i < (b); i++)
+#define per(i,a,b) for(int i = (a); i >= (b); i--)
+#define each(x,v) for(auto &x : v)
+#define read(v) for(auto &x : v) cin >> x;
+
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+const int INF = 1e18;
+
+void solve()
+{
+    int n; cin >> n;
+
+}   
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1; cin >> t;
+    while(t--)solve();
+}
